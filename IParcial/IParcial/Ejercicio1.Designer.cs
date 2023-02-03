1d75f9e5a0fbdaa822066033b76f8723d9f3e049
Eduardo Antonio Pérez <eduardoperez@gmail.com>
2023-02-03 11:55:25 -0600

    Commit Ejercicio 1
    Commit del Ejercicio1 ya finalizado

---
+++ IParcial/IParcial/Ejercicio1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace IParcial
+{
+    partial class Ejercicio1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NumeroTextBox = new System.Windows.Forms.TextBox();
+            this.MostrarButton = new System.Windows.Forms.Button();
+            this.ResultadoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(139, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero:";
+            // 
+            // NumeroTextBox
+            // 
+            this.NumeroTextBox.Location = new System.Drawing.Point(216, 100);
+            this.NumeroTextBox.Name = "NumeroTextBox";
+            this.NumeroTextBox.Size = new System.Drawing.Size(118, 20);
+            this.NumeroTextBox.TabIndex = 1;
+            // 
+            // MostrarButton
+            // 
+            this.MostrarButton.Location = new System.Drawing.Point(216, 216);
+            this.MostrarButton.Name = "MostrarButton";
+            this.MostrarButton.Size = new System.Drawing.Size(78, 30);
+            this.MostrarButton.TabIndex = 2;
+            this.MostrarButton.Text = "Mostrar";
+            this.MostrarButton.UseVisualStyleBackColor = true;
+            this.MostrarButton.Click += new System.EventHandler(this.MostrarButton_Click);
+            // 
+            // ResultadoLabel
+            // 
+            this.ResultadoLabel.AutoSize = true;
+            this.ResultadoLabel.Location = new System.Drawing.Point(165, 152);
+            this.ResultadoLabel.Name = "ResultadoLabel";
+            this.ResultadoLabel.Size = new System.Drawing.Size(0, 13);
+            this.ResultadoLabel.TabIndex = 3;
+            // 
+            // Ejercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 298);
+            this.Controls.Add(this.ResultadoLabel);
+            this.Controls.Add(this.MostrarButton);
+            this.Controls.Add(this.NumeroTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejercicio1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NumeroTextBox;
+        private System.Windows.Forms.Button MostrarButton;
+        private System.Windows.Forms.Label ResultadoLabel;
+    }
+}
+
---
+++ IParcial/IParcial/Ejercicio1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IParcial
+{
+    public partial class Ejercicio1 : Form
+    {
+        public Ejercicio1()
+        {
+            InitializeComponent();
+        }
+        int resultadofinal;
+       
+       
+        private void MostrarButton_Click(object sender, EventArgs e)
+        {
+            int numero = int.Parse(NumeroTextBox.Text);
+
+            
+
+            resultadofinal = Negativo(numero);
+            resultadofinal = Positivo(numero);
+        }
+
+        private int Positivo(int numero)
+        {
+            if(numero % 2==0 && numero>=0 ) 
+            {
+                ResultadoLabel.Text = "El numero " +numero +" es PAR y POSITIVO ";
+            }
+            else if(numero>=0)
+            {
+                ResultadoLabel.Text = "El numero " + numero + " es IMPAR y POSITIVO";
+            }
+            return numero;
+            
+        }
+        private int Negativo(int numero)
+        {
+            
+            if(numero %2==0 && numero<0 )
+                
+            {
+                ResultadoLabel.Text = "El numero " + numero + " es PAR y NEGATIVO";
+            }
+            else
+            {
+                ResultadoLabel.Text = "El numero " + numero + " es IMPAR y NEGATIVO";
+            }
+            return numero;
+        }
+       
+    }
+}
